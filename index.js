1f2d5302d18aee95fa5876f100ec83e763d798ca
csesopnil <sopnilstar@gmail.com>
2023-11-21 17:54:24 +0600

    added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const os=require("os")
+os.hostname=()=>"localhost"
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -8,7 +10,7 @@ const port = process.env.PORT || 5000;
 //dot environment config
 require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
+ 
 // middlewars
 app.use(cors());
 app.use(express.json());
